1490b3642e66ef4e39b263fe5f32766295c6759a
DevOpsUsuGit <javendanob@intergrupo.com>
2020-01-15 10:53:02 -0500

    add componente

---
+++ WebApplication1.Shared/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Shared
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
